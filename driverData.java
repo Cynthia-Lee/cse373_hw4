f019431f44ce17df2044ba262c8de264ad38a0bf
Cynthia Lee <35241156+Cynthia-Lee@users.noreply.github.com>
2019-11-14 16:19:33 -0500

    Add files via upload

--- driverData.java
+++ driverData.java
@@ -8,9 +8,6 @@ import java.io.FileReader;
 import java.util.Comparator;
 import java.util.Collections;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-
 // import java.util.HashSet;
 // import java.util.Set;
 
@@ -21,10 +18,11 @@ public class driverData {
         // second line will contain the number of subsets
         // each line after that will contain the associated subset
 
-        // File file1 = new File("./s-X-12-6.txt");
-        File file1 = new File("./s-rg-31-15.txt");
-        // File file1 = new File("./s-rg-63-25.txt");
+        File file1 = new File("./s-k-30-50.txt");
+        
+        // File file1 = new File("./s-k-20-30.txt");
         // File file1 = new File("./test.txt");
+        
         ArrayList<ArrayList<Integer>> subsets = new ArrayList<>(); // arraylist of all the subests
         int universal;
         int numSubsets;
@@ -48,35 +46,25 @@ public class driverData {
         }
 
         System.out.println(file1.getName());
-        System.out.println();
+        // System.out.println();
 
-        Calendar cal = Calendar.getInstance();
-        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-        String b = sdf.format(cal.getTime());
-        System.out.println(sdf.format(cal.getTime()));
-
-        System.out.println();
+        long startTime = System.nanoTime();
 
         // ArrayList<ArrayList<Integer>> result = new ArrayList<>(); // result
         // boolean[] a = new boolean[numSubsets]; // current combination
         // subsets = data... the subsets given (ints given)
         // universal = numbers needed to be covered
-        ArrayList<ArrayList<Integer>> start = new ArrayList<>(); // result
+        
+        // ArrayList<ArrayList<Integer>> start = new ArrayList<>(); // result
         minSetCover f1 = new minSetCover(universal, subsets);
-        f1.backtrack(start, f1.data);
+        // f1.backtrack(start);
+        f1.backtrack(f1.a);
         System.out.println("Minimum set cover is:");
         System.out.println(Arrays.toString(f1.currSol.toArray()));
         System.out.println("Size of MSC is: " + f1.currSol.size());
-        System.out.println();
-
-        System.out.println(b);
-        cal = Calendar.getInstance();
-        SimpleDateFormat endTime = new SimpleDateFormat("HH:mm:ss");
-        System.out.println(endTime.format(cal.getTime()));
-
-        System.out.println(f1.test);
-
-        // long difference = endTime.getTime() - sdf.getTime();
+        
+        System.out.println("Program running time: " + (System.nanoTime() - startTime)/1000000000.0 + "s");
+        System.out.println("Number of comparisons: " + f1.test);
     }
 }
 
@@ -92,9 +80,6 @@ class minSetCover {
         this.universal = universal;
         this.data = new ArrayList<ArrayList<Integer>>(data); // clone;
 
-        
-        System.out.println("data = " + Arrays.toString(this.data.toArray()));
-
         // sort data subsets(array lists) from longest to smallest
         Collections.sort(this.data, new Comparator<ArrayList<Integer>>() {
             public int compare(ArrayList<Integer> a1, ArrayList<Integer> a2) {
@@ -102,14 +87,44 @@ class minSetCover {
             }
         });
 
-        System.out.println("data = " + Arrays.toString(this.data.toArray()));
+        // early on
+        // System.out.println("data " + Arrays.toString(this.data.toArray()));
+
+        ArrayList<ArrayList<Integer>> contained = new ArrayList<>();
+        for (int k = 0; k < universal; k++) {
+            ArrayList<Integer> empty = new ArrayList<>();
+            contained.add(empty);
+        }
+        // universal counting of data index
+        for (int w = 0; w < this.data.size(); w++) { // each subset
+            ArrayList<Integer> currSubset = this.data.get(w);
+            for (int j = 0; j < currSubset.size(); j++) { // inside the subset
+                int num = currSubset.get(j); // each number in the subset
+                contained.get(num-1).add(w); // add subset's index in data                
+            }
+        }
         
+        // System.out.println("contained " + Arrays.toString(contained.toArray()));
+        // System.out.println();
+
+        for (int w = 0; w < contained.size(); w++) { // contained
+            if (contained.get(w).size() == 1) { 
+                // must be added to the solution
+                int index = contained.get(w).get(0);
+                // System.out.println(index);
+                this.a.add(this.data.get(index));
+                // System.out.println("d arr " + Arrays.toString(this.data.toArray()));
+                // System.out.println("this arr " + Arrays.toString(this.a.toArray()));
+                this.data.remove(this.data.get(index));
+                // System.out.println("data arr " + Arrays.toString(this.data.toArray()));
+            }
+        }
     }
 
     // BACKTRACK
     int test;
 
-    public void backtrack(ArrayList<ArrayList<Integer>> a, ArrayList<ArrayList<Integer>> data) {
+    public void backtrack(ArrayList<ArrayList<Integer>> a) {
         // a = current combination
         // universal, need numbers 1 - universal
         // check is the universal cover check
@@ -129,35 +144,32 @@ class minSetCover {
         }
 
         // System.out.println(Arrays.toString(this.check));
-        System.out.println("...testing " + Arrays.toString(a.toArray()));
-        System.out.println(++this.test); // 127
+        // System.out.println("...testing " + Arrays.toString(a.toArray()));
+        this.test++;
         if (isASolution(this.check)) {
-            // System.out.println("...testing " + Arrays.toString(a.toArray()));
-            // System.out.println(Arrays.toString(this.check));
-            // System.out.println(++this.test); // 64
             processSolution(a);
         } else {
             // CONSTRUCT CANDIDTATES
 
-            if (!currSol.isEmpty() && (a.size() > this.currSol.size())) { // if current perm is longer than sol found
+            if (!currSol.isEmpty() && (a.size() >= this.currSol.size())) { 
+                // if current perm is longer than sol found
                 return; // candidates is an empty array
             }
 
             // get rid of last seen
             int number = 0;
             if (!a.isEmpty()) {
-                // number = candidates.indexOf(a.get(a.size() - 1));
-                number = data.indexOf(a.get(a.size() - 1));
-                // System.out.println("HELLO JO " + number);
+                if (this.data.indexOf(a.get(a.size() - 1)) == -1) {
+                    number = 0;
+                } else { 
+                    number = this.data.indexOf(a.get(a.size() - 1));
+                }
             }
 
-            int uncovered = 0;
-            int maxUncovered = 0;
+            for (int w = number; w < this.data.size(); w++) { // each subset
+                ArrayList<Integer> currSubset = this.data.get(w);
 
-            for (int w = number; w < data.size(); w++) { // each subset
-                ArrayList<Integer> currSubset = data.get(w);
-
-                if (!a.contains(data.get(w))) { // if it is not in the subset already
+                if (!a.contains(this.data.get(w))) { // if it is not in the subset already
                     for (int j = 0; j < currSubset.size(); j++) { // inside the subset
                         int num = currSubset.get(j); // each number in the subset
                         // amount of uncovered elements
@@ -167,47 +179,24 @@ class minSetCover {
                             break;
                         } 
                     } // done with the subset
-                    
-                    /*
-                    System.out.println("this " + Arrays.toString(currSubset.toArray()));
-                    System.out.println(maxUncovered);
-                    // System.out.println(uncovered);
-                    
-                    if (maxUncovered < uncovered) {
-                        System.out.println("max " + Arrays.toString(currSubset.toArray()));
-                        candidates.add(0, currSubset);
-                        maxUncovered = uncovered;
-                        // System.out.println("c " + Arrays.toString(candidates.toArray()));
-                    } else {
-                        candidates.add(currSubset);
-                    }
-                    uncovered = 0;
-                    */
                 }
             }
-
-            // We add the subset containing the largest number of remaining uncovered
-            // elements repeatedly until all are covered.
-
             // end of construct candidates
 
-            System.out.println("a is " + Arrays.toString(a.toArray()));
-            System.out.println("candidates are " + Arrays.toString(candidates.toArray()));
+            // System.out.println("a is " + Arrays.toString(a.toArray()));
+            // System.out.println("candidates are " + Arrays.toString(candidates.toArray()));
 
             for (i = 0; i < candidates.size(); i++) {
-                // a.add(candidates.get(i)); // add subset to current combination
-
+                
                 // make move
-                a.add(candidates.get(i));
+                a.add(candidates.get(i)); // add subset to current combination
 
                 // System.out.println("a is " + Arrays.toString(a.toArray()));
                 // System.out.println("check is " + Arrays.toString(this.check));
-
-                System.out.println("backtrack next");
-                System.out.println("");
+                // System.out.println("");
 
                 // backtrack
-                backtrack(a, data);
+                backtrack(a);
 
                 // unmake move
                 a.remove(a.size() - 1);
@@ -231,10 +220,4 @@ class minSetCover {
         }
     }
 
-    /*
-     * public boolean andCheck(ArrayList<boolean[]> a) { // like bitwise and // and
-     * each boolean array. and each subset
-     * 
-     * }
-     */
 }
